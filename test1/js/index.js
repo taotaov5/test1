bf026c485e8b6b74b600baf6ab1f872994d79655
f z <565009567@qq.com>
2020-02-28 22:01:40 +0800

    first commit

---
+++ test1/js/index.js
@@ -0,0 +1,8 @@
+function prevous(){
+			var if_paging=document.getElementById("if_paging");
+			if_paging.src="paging/one.html";
+		}
+		function next(){
+			var if_paging=document.getElementById("if_paging");
+			if_paging.src="paging/two.html";
+		}
